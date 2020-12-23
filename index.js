e09b2e615a32e4f87619a1d4069e5a0c81a88bd3
Julian Pollinger <neferin42@gmail.com>
2020-12-23 14:42:23 +0100

    License und user Count

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const PORT = 3420;
-const VERSION = "0.1.0";
-const Express = require("express")();
-const Http = require("http").Server(Express);
-const io = require("socket.io")(Http, {
+import express from "express";
+
+import {Server} from "http";
+
+import {Server as SocketServer} from "socket.io";
+
+const Express = express();
+const Http = Server(Express);
+const io = new SocketServer(Http, {
     cors: {
         origin: '*'
     }
 });
 
 
+const PORT = 3420;
+const VERSION = "0.1.0";
+let USERCOUNT = 0;
+
 const helo = {
-    version: VERSION
+    version: VERSION,
+    userCount: USERCOUNT
 }
 
 function getID(length = 16) {
     return '_' + Math.random().toString(36).substr(2, length);
 }
 
-
 io.on("connection", socket => {
     const id = getID();
+    USERCOUNT++;
     console.log(`user ${id} connected`);
+    console.log(`${USERCOUNT} users connected`);
+
     socket.emit("helo", {
         id,
         ...helo
@@ -30,7 +41,9 @@ io.on("connection", socket => {
 
     });
     socket.on('disconnect', () => {
+        USERCOUNT--;
         console.log(`user ${id} disconnected`);
+        console.log(`${USERCOUNT} users connected`);
     });
 });
 
